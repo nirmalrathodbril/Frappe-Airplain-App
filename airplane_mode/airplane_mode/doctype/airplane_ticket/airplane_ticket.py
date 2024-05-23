129998f7e3604593502187b1603dea4b27140151
Nirmal <nirmal.r@brilworks.com>
2024-05-23 16:17:34 +0530

    Day-1 assignments

---
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2024, Nirmal Rathod and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+#from apps.frappe import frappe
+
+
+class AirplaneTicket(Document):
+	def validate(self):
+		print("======== AirplaneTicket validate 123")
+		add_ons = self.add_ons
+		print(add_ons)
+
+		# Dictionary to hold unique add_ons by name
+		unique_add_ons_dict = {}
+		for add_on in add_ons:
+			unique_add_ons_dict[add_on.item] = add_on
+
+		# Update the add_ons with the unique values
+		self.add_ons = list(unique_add_ons_dict.values())
+
+		print("====== Result ==============")
+		print(self.add_ons)
+
+
+	def validation_to_check(self):
+		print("before submit from Airplate ticket")
+		if (self.status != 'Boarded'):
+			frappe.throw("Ticket can not be submit as Flight has not been boarded yet.")
+
+
+	def before_save(self):
+		sum_amount = 0
+		for item in self.add_ons:
+			sum_amount += item.amount
+
+		self.total_amount = sum_amount + self.flight_price
+
+	def on_load(self):
+		sum_amount = 0
+		for item in self.add_ons:
+			sum_amount += item.amount
+
+		self.total_amount = sum_amount + self.flight_price
+
