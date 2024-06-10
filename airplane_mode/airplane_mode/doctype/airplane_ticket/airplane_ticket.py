4832b031bf3435591ddb5500ebd9223ec32ebb10
Nirmal <nirmal.r@brilworks.com>
2024-06-10 10:38:24 +0530

    Created new controller to throw an error if capacity of airplane has just reach.

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -2,8 +2,10 @@
 # For license information, please see license.txt
 import random
 import string
+
 import frappe
 from frappe.model.document import Document
+
 
 #from apps.frappe import frappe
 
@@ -26,14 +28,48 @@ class AirplaneTicket(Document):
 		print(self.add_ons)
 		self.validation_to_check()
 
-
 	def validation_to_check(self):
 		print("before submit from Airplate ticket")
 		if (self.status != 'Boarded'):
 			frappe.throw("Ticket can not be submit as Plain has not been boarded yet.")
 
+	def validation_to_available_seats(self):
+		# airplane ticket --> airplane flight --> airplane
+		print(self.as_dict())
+		docFlight = self.flight
+		print("Insideeeeee")
+		print(docFlight.as_dict())
+		docAirplane = frappe.get_doc("Airplane flight", docFlight.flight)
+		print(docAirplane.as_dict())
+		seatCount = docAirplane.capacity
+		print(seatCount)
+
+		# get number of tickets from airplane tickets having seat assigned
+		assignedSeatCount = 20
+
+		frappe.throw("Oops, Sold Out!")  # validation_to_available_seats
+
+	def check_seat_availability(self):
+		# Fetch the Airplane Flight document
+		airplane_flight = frappe.get_doc("Airplane Flight", self.flight)
+
+		# Fetch the Airplane document linked to the Airplane Flight
+		airplane = frappe.get_doc("Airplane", airplane_flight.airplane)
+		capacity = airplane.capacity
+
+		# Count the existing tickets for the same Airplane Flight
+		existing_tickets = frappe.db.count("Airplane Ticket",
+										   filters={"flight": self.flight})
+
+		# Check if the new ticket exceeds the capacity
+		if existing_tickets >= capacity:
+			frappe.throw("Oops, Sold Out!")
 
 	def before_save(self):
+
+		## self.validation_to_available_seats()
+		self.check_seat_availability()
+
 		sum_amount = 0
 		for item in self.add_ons:
 			sum_amount += item.amount
@@ -51,5 +87,5 @@ class AirplaneTicket(Document):
 		number = random.randint(1, 99)
 		# Generate a random uppercase letter
 		letter = random.choice(string.ascii_uppercase)
-		# Combine number and letter to form seat assignment
-		# self.seat = f"{number}{letter}"
+	# Combine number and letter to form seat assignment
+	# self.seat = f"{number}{letter}"
