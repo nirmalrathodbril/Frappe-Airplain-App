835ae6b89fe01b0c5ed22cdfc26bcc96f965e7db
Nirmal <nirmal.r@brilworks.com>
2024-06-11 14:10:31 +0530

    Empty files for revenue by airline Script report

---
+++ airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Nirmal Rathod and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
