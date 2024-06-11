fb7201d19baf377cd6050fbca4dc4bd53d4b348d
Nirmal <nirmal.r@brilworks.com>
2024-06-11 17:56:10 +0530

    Revenue by Airline with chart donut

--- airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
+++ airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
@@ -6,33 +6,49 @@ import frappe
 
 
 def execute(filters=None):
-	print("============================= REPORT")
 	columns = [
 		{
-			"fieldName": "airline",
+			"fieldname": "airline",
 			"label": "Airline",
-			"fieldtype": "data"
+			"fieldtype": "link"
 		},
 		{
-			"fieldName": "total_revenue",
+			"fieldname": "total_revenue",
 			"label": "Total Revenue",
 			"fieldtype": "Currency",
-			"Options": "AED"
+			"options": "AED"
 		}
 	]
 	data = get_airline_revenue()
-	return columns, data
+
+	chart = get_chart(data)
+	frappe.errprint(chart)
+
+	return columns, data, "Here is the report", chart
+
+
+def get_chart(data):
+	chart = {
+		"data": {
+			"labels": [x['airline'] for x in data],
+			"datasets": [{
+				"values": [x['total_revenue'] for x in data]
+			}],
+		},
+		"type": "donut",
+	}
+	return chart
 
 
 def get_airline_revenue():
-    # Fetch all airlines
-    airlines = frappe.get_all('Airline', fields=['name'])
+	# Fetch all airlines
+	airlines = frappe.get_all('Airline', fields=['name'])
 
-    data = []
+	data = []
 
-    for airline in airlines:
-        # Fetch total revenue for each airline
-        revenue = frappe.db.sql("""
+	for airline in airlines:
+		# Fetch total revenue for each airline
+		revenue = frappe.db.sql("""
             SELECT SUM(at.flight_price) as total_revenue
             FROM `tabAirplane Ticket` at
             JOIN `tabAirplane Flight` af ON at.flight = af.name
@@ -40,13 +56,13 @@ def get_airline_revenue():
             WHERE a.airline = %s
         """, (airline['name'],), as_list=True)
 
-        # Ensure that the result is valid
-        total_revenue = revenue[0][0] if revenue and revenue[0][0] is not None else 0
+		# Ensure that the result is valid
+		total_revenue = revenue[0][0] if revenue and revenue[0][0] is not None else 0
 
-        # Append the result to data list
-        data.append({
-            "airline": airline['name'],
-            "total_revenue": total_revenue
-        })
+		# Append the result to data list
+		data.append({
+			"airline": airline['name'],
+			"total_revenue": total_revenue
+		})
 
-    return data
+	return data
